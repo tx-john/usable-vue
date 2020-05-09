7882d8baf88a07476f6cfd7e0571e91e3b6b82d1
John Barry <jb3003@gmail.com>
2020-05-09 11:10:24 -0600

    Explicitly add AriaSlider as an exported component

--- src/lib-components/index.js
+++ src/lib-components/index.js
@@ -1,3 +1,6 @@
 /* eslint-disable import/prefer-default-export */
 export { default as UsableVueSample } from './usable-vue-sample.vue';
+
 export { default as List } from './list/List.vue';
+
+export { default as AriaSlider } from './input/AriaSlider.vue';
